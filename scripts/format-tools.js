28ebe4c489c4bf76b8aea8aa847d898362e30266
George Francis <georgefrancis105@gmail.com>
2016-06-01 16:07:50 +1200

    Rebuild (#1)
    * changed index layout
    
    * built new wire frame with scrollable content
    
    * embedded blogs into index page

--- scripts/format-tools.js
+++ scripts/format-tools.js
@@ -4,96 +4,107 @@
 console.log("initial test");
 
 var blogLinks = {
-  "technical": {
-    "t2_html_css_dom_p1":{
-      "title":"HTML CSS and the DOM Part 1",
-      "url":"blog/t2-html-css-dom-p1.html",
-      "created":"2016-04-15"
-    },
-    "t2_html_css_dom_p2":{
-      "title":"HTML CSS and the DOM Part 2",
-      "url":"blog/t2-html-css-dom-p2.html",
-      "created":"2016-04-15"
-    },
-    "t3_design_to_web_blog":{
-      "title":"Design to web blog",
-      "url":"blog/t3-design-to-web-blog.html",
-      "created":"2016-04-21"
-    },
-    "t4_javascript_basics":{
-      "title":"JavaScript Basics",
-      "url":"blog/t4-javascript-basics.html",
-      "created":"2016-04-29"
-    },
-    "t5_problem_solving":{
-      "title":"Problem Solving",
-      "url":"blog/t5-problem-solving.html",
-      "created":"2016-05-07"
-    },
-    "t6_js_language":{
-      "title":"JavaScript Language",
-      "url":"blog/t6-js-language.html",
-      "created":"2016-05-14"
-    },
-    "t8-testing":{
-      "title":"Scope",
-      "url":"blog/t8-testing.html",
-      "created":"2016-05-26"
-    }
+  "t2_html_css_dom_p1":{
+    "type":"technical",
+    "title":"HTML CSS and the DOM Part 1",
+    "url":"blog/t2-html-css-dom-p1.html",
+    "created":"2016-04-15"
   },
-  "cultural":{
-    "c1_reflection_blog":{
-      "title":"Reflection",
-      "url":"blog/c1-reflection-blog.html",
-      "created":"2016-04-06"
-    },
-    "c1_time_and_habits_blog":{
-      "title":"Time and Habits",
-      "url":"blog/c1-time-and-habits-blog.html",
-      "created":"2016-04-06"
-    },
-    "c2_emotional_intelligence":{
-      "title":"Emotional Intelligence",
-      "url":"blog/c2-emotional-intelligence.html",
-      "created":"2016-04-16"
-    },
-    "c3_meditation_process":{
-      "title":"Mediataion Process",
-      "url":"blog/c3-meditation-process.html",
-      "created":"2016-04-21"
-    },
-    "c4_daily_mindfulness":{
-      "title":"Daily Mindfulness",
-      "url":"blog/c4-daily-mindfulness.html",
-      "created":"2016-04-14"
-    },
-    "c5_self_confidence":{
-      "title":"Self Confidence",
-      "url":"blog/c5-self-confidence.html",
-      "created":"2016-05-04"
-    },
-    "c6_control":{
-      "title":"Control",
-      "url":"blog/c6-control.html",
-      "created":"2016-05-14"
-    },
-    "c7_motivation":{
-      "title":"Motivation",
-      "url":"blog/c7-motivation.html",
-      "created":"2016-05-22"
-    },
-    "c8_connecting":{
-      "title":"Connecting",
-      "url":"blog/c8-connecting.html",
-      "created":"2016-05-29"
-    }
+  "t2_html_css_dom_p2":{
+    "type":"technical",
+    "title":"HTML CSS and the DOM Part 2",
+    "url":"blog/t2-html-css-dom-p2.html",
+    "created":"2016-04-15"
   },
-  "projects":{
-    "javascript_racer":{
-      "title":"Racer Game",
-      "url":"http://georgef105.github.io/javascript-racer/",
-      "created":"2016-05-03"
-    }
+  "t3_design_to_web_blog":{
+    "type":"technical",
+    "title":"Design to web blog",
+    "url":"blog/t3-design-to-web-blog.html",
+    "created":"2016-04-21"
+  },
+  "t4_javascript_basics":{
+    "type":"technical",
+    "title":"JavaScript Basics",
+    "url":"blog/t4-javascript-basics.html",
+    "created":"2016-04-29"
+  },
+  "t5_problem_solving":{
+    "type":"technical",
+    "title":"Problem Solving",
+    "url":"blog/t5-problem-solving.html",
+    "created":"2016-05-07"
+  },
+  "t6_js_language":{
+    "type":"technical",
+    "title":"JavaScript Language",
+    "url":"blog/t6-js-language.html",
+    "created":"2016-05-14"
+  },
+  "t8-testing":{
+    "type":"technical",
+    "title":"Scope",
+    "url":"blog/t8-testing.html",
+    "created":"2016-05-26"
+  },
+  "c1_reflection_blog":{
+    "type":"cultural",
+    "title":"Reflection",
+    "url":"blog/c1-reflection-blog.html",
+    "created":"2016-04-06"
+  },
+  "c1_time_and_habits_blog":{
+    "type":"cultural",
+    "title":"Time and Habits",
+    "url":"blog/c1-time-and-habits-blog.html",
+    "created":"2016-04-06"
+  },
+  "c2_emotional_intelligence":{
+    "type":"cultural",
+    "title":"Emotional Intelligence",
+    "url":"blog/c2-emotional-intelligence.html",
+    "created":"2016-04-16"
+  },
+  "c3_meditation_process":{
+    "type":"cultural",
+    "title":"Mediataion Process",
+    "url":"blog/c3-meditation-process.html",
+    "created":"2016-04-21"
+  },
+  "c4_daily_mindfulness":{
+    "type":"cultural",
+    "title":"Daily Mindfulness",
+    "url":"blog/c4-daily-mindfulness.html",
+    "created":"2016-04-14"
+  },
+  "c5_self_confidence":{
+    "type":"cultural",
+    "title":"Self Confidence",
+    "url":"blog/c5-self-confidence.html",
+    "created":"2016-05-04"
+  },
+  "c6_control":{
+    "type":"cultural",
+    "title":"Control",
+    "url":"blog/c6-control.html",
+    "created":"2016-05-14"
+  },
+  "c7_motivation":{
+    "type":"cultural",
+    "title":"Motivation",
+    "url":"blog/c7-motivation.html",
+    "created":"2016-05-22"
+  },
+  "c8_connecting":{
+    "type":"cultural",
+    "title":"Connecting",
+    "url":"blog/c8-connecting.html",
+    "created":"2016-05-29"
+  },
+  "javascript_racer":{
+    "type":"project",
+    "title":"Racer Game",
+    "url":"http://georgef105.github.io/javascript-racer/",
+    "created":"2016-05-03"
   }
 };
 
@@ -112,4 +123,36 @@ function addLinks(){
   }
   // $("#blog-links").append("<h5>Blogs</h5>");
 }
-addLinks();
+//addLinks();
+
+var menuItemTemplate = '<div class="menu-item" id="menu-item-%id%"><div class="menu-item-type">%type%</div><div class="menu-item-date">%date%</div><div class="menu-item-title">%title%</div></div>';
+
+function buildMenu(sortBy){
+  sortBy = sortBy || "none";
+  //console.log("buildMenu sortBy: " + sortBy);
+  for(var key in blogLinks){
+    //console.log(key)
+    $("#menuItems").append(menuItemTemplate.replace("%type%",blogLinks[key].type.charAt(0).toUpperCase() + blogLinks[key].type.slice(1)).replace("%date%",blogLinks[key].created).replace("%title%",blogLinks[key].title).replace("%id%",key));
+    // $("#menu-item-"+key).click(function(key){buildContent(key);});
+    $("#menu-item-"+key).click({key:key}, buildContent);
+  }
+}
+
+buildMenu();
+
+function buildContent(event){
+  console.log("buildContent: "+event.data.key);
+  //console.log("url: " + )
+  // $("#linkContent").load(blogLinks[event.data.key].url + " div#actualContent");
+
+  $.ajax({
+    url:blogLinks[event.data.key].url,
+    type:'GET',
+    success: function(data){
+      $("#linkContent").html($(data).filter("#actualContent").html() || $(data).find("#actualContent").html() );
+      //console.log($(data).filter("#actualContent").html());
+      //console.log(data);
+    }
+  });
+
+}
