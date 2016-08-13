5d5e5096bc6a13aa2d8abd7107762c4d51d06b8e
george francis <georgefrancis105@gmail.com>
2016-08-13 17:36:51 +1200

    Make Contact page, make about and contact page routes

---
+++ app/components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default (props) => {
+  return (
+    <div className='container content home'>
+      <h2>Contact</h2>
+    </div>
+  )
+}
--- app/components/routes.js
+++ app/components/routes.js
@@ -4,12 +4,16 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './App'
 import Projects from './containers/ProjectsContainer'
 import Home from './Home'
+import About from './About'
+import Contact from './Contact'
 
 export default (
   <Router history={browserHistory}>
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
       <Route path='/projects' component={Projects} />
+      <Route path='/about' component={About} />
+      <Route path='/contact' component={Contact} />
     </Route>
   </Router>
 )
