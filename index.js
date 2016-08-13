b29f9c07779ba5fd068e0df14ef7c116586b5a64
george francis <georgefrancis105@gmail.com>
2016-08-13 14:58:16 +1200

    Setup redux-thunk

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
 
 import reducer from './app/reducer'
 import routes from './app/components/routes'
 
 const store = createStore(
-  reducer
+  reducer,
+  applyMiddleware(
+    thunkMiddleware
+  )
 )
 
 render((
