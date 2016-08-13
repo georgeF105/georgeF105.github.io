f4c3299e10d29bfaa1f1b837f776d3b98397dfdf
george francis <georgefrancis105@gmail.com>
2016-08-13 16:36:43 +1200

    Make header links

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default (props) => {
   return (
     <div className='header'>
       <div className='container'>
-        <h1>George Francis</h1>
+        <h1 className='title'>George Francis</h1>
+        <div className='header-links'>
+          <Link to='/projects' activeClassName='active-link'>Projects</Link>
+          <Link to='/about' activeClassName='active-link'>About</Link>
+          <Link to='/contact' activeClassName='active-link'>Contact</Link>
+        </div>
       </div>
       <hr />
     </div>
