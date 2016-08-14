44e8806d6f15824a7d2536d65f948ab4bd074c82
george francis <georgefrancis105@gmail.com>
2016-08-14 17:48:13 +1200

    Updates

--- bundle.js
+++ bundle.js
@@ -28142,7 +28142,7 @@
 
 	exports.default = _react2.default.createElement(
 	  _reactRouter.Router,
-	  { history: _reactRouter.browserHistory },
+	  { history: _reactRouter.hashHistory },
 	  _react2.default.createElement(
 	    _reactRouter.Route,
 	    { path: '/', component: _App2.default },
